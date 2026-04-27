c32cf77b77f50bc8a3d62b1fc4eb8dfb55dd6360
YManishKumar <49333311+YManishKumar@users.noreply.github.com>
2026-04-27 17:48:21 +0530

    UPADTED ENV

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.chesssarena.store/',
+  apiUrl: 'https://api.chesssarena.store',
   supabaseUrl: 'https://czapupmvtlzqfkzsuvft.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6YXB1cG12dGx6cWZrenN1dmZ0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcxOTE1NzAsImV4cCI6MjA5Mjc2NzU3MH0.hJkTw5qzmV55Ln566mYJUEGtuKsSaE65MtrYgM5RVrA'
 };
